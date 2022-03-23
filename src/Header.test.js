6ed784efc8775fc0db4234898a461779c13cb241
pannachow <chowpanna@gmail.com>
2022-03-23 14:48:07 +0100

    add github pages

--- src/Header.test.js
+++ src/Header.test.js
@@ -2,7 +2,7 @@ import { screen } from "@testing-library/react";
 import Header from "./Header";
 import { renderWithRouterAndTheme } from "./testUtils";
 
-test("contains website title", () => {
+test("contains product title", () => {
   renderWithRouterAndTheme(<Header />);
   const title = screen.getByText(/SlapSticker/i);
   expect(title).toBeInTheDocument();
